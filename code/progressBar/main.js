714a0a1ee2f55c4f3508aa508ce0ce85b76bc206
imdsoho <imdsoho@gmail.com>
2026-02-03 12:47:27 +0900

    commit

---
+++ code/progressBar/main.js
@@ -0,0 +1,40 @@
+window.onload = function onLoad() {
+    /*let circle = new ProgressBar.Circle('#progress', {
+        color: '#FCB03C',
+        duration: 3000,
+        easing: 'easeInOut'
+    });
+    circle.animate(1);*/
+
+
+    let bar = new ProgressBar.Line('#progress', {
+        strokeWidth: 4,
+        easing: 'easeInOut',
+        duration: 10000,
+        color: '#FFEA82',
+        trailColor: '#eee',
+        trailWidth: 1,
+        svgStyle: {width: '100%', height: '100%'},
+        text: {
+            style: {
+                // Text color.
+                // Default: same as stroke color (options.color)
+                color: '#999',
+                position: 'absolute',
+                right: '0',
+                top: '30px',
+                padding: 0,
+                margin: 0,
+                transform: null
+            },
+            autoStyleContainer: false
+        },
+        from: {color: '#FFEA82'},
+        to: {color: '#ED6A5A'},
+        step: (state, bar) => {
+            bar.setText(Math.round(bar.value() * 100) + ' %');
+        }
+    });
+
+    bar.animate(1.0);
+};
